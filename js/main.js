0ce5d5856bf8b06e08e49a06b553ddb663e7c6d5
aishaa89 <ayyshh033@gmail.com>
2023-12-23 15:32:45 +0200

    update

--- js/main.js
+++ js/main.js
@@ -110,8 +110,7 @@ function validateName(){
 
 function validateUrl(){
     var validsiteUrl=siteUrl.value;
-    var regexUrl=  /^(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g;
-    ;
+    var regexUrl=/^(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})(\.[a-zA-Z0-9]{2,})?\/$/
 
     if(regexUrl.test(validsiteUrl)){
         siteUrl.classList.add("is-valid");
